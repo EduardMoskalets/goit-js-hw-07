3584f135a916b1dbdbd2d478d6e2f6cbbff68de8
Eduard <edik.moskalets@gmail.com>
2024-05-19 19:40:04 +0300

    task5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyElem = document.querySelector('body');
+const btnElem = document.querySelector('.change-color');
+const spanColor = document.querySelector('.color');
+
+btnElem.addEventListener('click', changeColor => {
+  bodyElem.setAttribute('style', `background-color: ${getRandomHexColor()}`);
+  spanColorElem.textContent = body.getRandomColor(`background-color`);
+});
